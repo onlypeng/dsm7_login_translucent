63a918a32ae4f03f9909a16597b562988debba3c
onlypeng <15733181556@163.com>
2022-09-17 15:29:18 +0800

    修复个别字体显示不清

--- dsm7.1.1/install.sh
+++ dsm7.1.1/install.sh
@@ -14,21 +14,20 @@ do
                 mkdir ${script_dir}/backups
                 echo "第一次运行，开始备份原文件"
                 cp /usr/syno/synoman/webman/login/dist/style.css ${script_dir}/backups/style.css
-                echo ""
                 cp /usr/syno/synoman/webman/login/dist/style.css.gz ${script_dir}/backups/style.css.gz
-                echo ""
+                cp /usr/syno/synoman/webman/login/dist/6.style.css ${script_dir}/backups/6.style.css
+                cp /usr/syno/synoman/webman/login/dist/6.style.css.gz ${script_dir}/backups/6.style.css.gz
                 cp /usr/syno/synoman/webman/login/dist/assets/1bc43875501e55e5e741e427ca50dbdf.png ${script_dir}/backups/1bc43875501e55e5e741e427ca50dbdf.png
-                echo ""
                 cp /usr/syno/synoman/webman/3rdparty/SecureSignIn/login-dist/3.SecureSignInLogin.css ${script_dir}/backups/3.SecureSignInLogin.css
-                echo ""
                 cp /usr/syno/synoman/webman/3rdparty/SecureSignIn/login-dist/assets/68b2c5f5747c3085bba5dd628b90ae0f.gif ${script_dir}/backups/68b2c5f5747c3085bba5dd628b90ae0f.gif
-                echo ""
                 cp /usr/syno/synoman/webman/3rdparty/SecureSignIn/login-dist/assets/addc28996dad298560fd62666efd5c0b.gif ${script_dir}/backups/addc28996dad298560fd62666efd5c0b.gif
                 echo "文件备份完成，请勿删除《 backups 》目录和目录内文件"
             fi
             echo "开始替换文件"
             cp ${script_dir}/patch/style.css /usr/syno/synoman/webman/login/dist/style.css
             cp ${script_dir}/patch/style.css.gz /usr/syno/synoman/webman/login/dist/style.css.gz
+            cp ${script_dir}/patch/6.style.css /usr/syno/synoman/webman/login/dist/6.style.css
+            cp ${script_dir}/patch/6.style.css.gz /usr/syno/synoman/webman/login/dist/6.style.css.gz
             cp ${script_dir}/patch/1bc43875501e55e5e741e427ca50dbdf.png /usr/syno/synoman/webman/login/dist/assets/1bc43875501e55e5e741e427ca50dbdf.png
             cp ${script_dir}/patch/3.SecureSignInLogin.css /usr/syno/synoman/webman/3rdparty/SecureSignIn/login-dist/3.SecureSignInLogin.css
             cp ${script_dir}/patch/68b2c5f5747c3085bba5dd628b90ae0f.gif /usr/syno/synoman/webman/3rdparty/SecureSignIn/login-dist/assets/68b2c5f5747c3085bba5dd628b90ae0f.gif
@@ -40,11 +39,12 @@ do
             echo "开始还原文件"
             cp ${script_dir}/backups/style.css /usr/syno/synoman/webman/login/dist/style.css
             cp ${script_dir}/backups/style.css.gz /usr/syno/synoman/webman/login/dist/style.css.gz
+            cp ${script_dir}/backups/6.style.css /usr/syno/synoman/webman/login/dist/style.css
+            cp ${script_dir}/backups/6.style.css.gz /usr/syno/synoman/webman/login/dist/style.css.gz
             cp ${script_dir}/backups/1bc43875501e55e5e741e427ca50dbdf.png /usr/syno/synoman/webman/login/dist/assets/1bc43875501e55e5e741e427ca50dbdf.png
             cp ${script_dir}/backups/3.SecureSignInLogin.css /usr/syno/synoman/webman/3rdparty/SecureSignIn/login-dist/3.SecureSignInLogin.css
             cp ${script_dir}/backups/68b2c5f5747c3085bba5dd628b90ae0f.gif /usr/syno/synoman/webman/3rdparty/SecureSignIn/login-dist/assets/68b2c5f5747c3085bba5dd628b90ae0f.gif
             cp ${script_dir}/backups/addc28996dad298560fd62666efd5c0b.gif /usr/syno/synoman/webman/3rdparty/SecureSignIn/login-dist/assets/addc28996dad298560fd62666efd5c0b.gif
-            rm -rf ${script_dir}/backups
             echo "还原完成，退出脚本"
             exit 1               
             ;;
