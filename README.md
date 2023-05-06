a5c8a0960691d85f0e45da79beec55b18bb73241
onlypeng <15733181556@163.com>
2023-05-06 22:11:05 +0800

    update

--- README.md
+++ README.md
@@ -1,8 +1,39 @@
-群晖7.x设置自动切换壁纸后白色输入框显示和突兀，所以改为半透明化很有必要 <br>
+修改群晖7.x登陆页面登陆框主题<br>
 <br>
+
 # 此补丁适用于DS918+，其他版本没测试过，请自行查看测试<br>
-# 具体操作查看各目录下说明文件<br>
-# 替换脚本可修改配置通用其他替换文件使用
+# 根据自己群辉版本安装对应补丁<br>
+frosting.sh 为磨砂玻璃效果<br>
+transparent.sh 为黑色透明效果<br>
+参数 1为替换，2为还原<br>
+例如磨砂效果安装：<br>
+    上传脚本的路径 可在File Station 右键点击install脚本-属性-位置 获取
+    方法1： <br>
+        0、上传脚本到群晖 <br>
+        1、开启ssh并进入群晖后台 <br>
+        2、sudo -i  切换root用户 <br>
+        3、cd 《上传脚本的路径》 <br>
+        4、chmod 755 install.sh 赋予脚本运行权限 <br>
+        5、./frosting.sh 1 运行脚本替换 <br>
+        6、./frosting.sh 2 运行脚本还原 <br>
+    方法2： <br>
+        0、上传脚本到群晖 <br>
+        1、打开控制面板-任务计划 <br>
+        2、新增-计划的任务-用户定义的脚本 <br>
+        3、常规：任务名称：随意写，用户账号选root，取消勾选已启动<br>
+        4、任务设置：运行命令：上传脚本的路径/frosting.sh 1 >> 上传脚本的路径/log.txt 2>&1 <br>
+        5、点击确定，右击创建的任务，运行，查看日志是否替换成功 <br>
+        6、还原补丁更改运行命令为：上传脚本的路径/frosting.sh 2 >> 上传脚本的路径/log.txt 2>&1 <br>
+<br>
+
+文件路径：<br>
+    gif: 批准登录动图原文件
+    patch：文件补丁
+    backups： 第一次替换后生成的目录，里面存放替换文件的备份
+    backups(未修改文件)： 给替换过文件，但没保存原文件的用，使用方法安装一次补丁，然后把backups(未修改文件)中文件复制并覆盖到backups文件夹中。
+<br>
+
+备注：本人ps菜的扣脚，gif动图存在瑕疵，会ps的大神可自己在在gif目录下载动图修改后替换patch文件中动图
 # 显示效果： <br>
 ![login2](https://user-images.githubusercontent.com/29589598/190339149-bd36ddc3-4a0b-4e2f-a87c-294d5ebd759a.png)
 ![login](https://user-images.githubusercontent.com/29589598/190339177-d97db048-71d4-4b62-a228-b9140cbc9236.png)
---
+++ dsm7.1.1/frosting.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+# 配置路径信息自动替换相关文件
+
+set -e 
+
+par=$1
+if  [[ -n $par && "$par" != "1" && "$par" != "2" ]]; then
+    echo "参数错误"
+    exit 1
+fi
+
+# 获取脚本位置
+script_dir=$(cd $(dirname $0);pwd)
+echo $script_dir
+echo "请勿删除当前目录下《 backups 》目录和目录内文件，如果脚本和文件更新请先利用当前脚本还原文件再重新替换。"
+echo "请勿删除当前目录下《 backups 》目录和目录内文件，如果脚本和文件更新请先利用当前脚本还原文件再重新替换。"
+echo "请勿删除当前目录下《 backups 》目录和目录内文件，如果脚本和文件更新请先利用当前脚本还原文件再重新替换。"
+
+
+patch_name="frosting"
+
+# 替换配置 [以patch为根，替换文件位置]=被替换的文件位置
+declare -A patch
+patch["style.css"]="/usr/syno/synoman/webman/login/dist/style.css"
+patch["style.css.gz"]="/usr/syno/synoman/webman/login/dist/style.css.gz"
+patch["3.SecureSignInLogin.css"]="/usr/syno/synoman/webman/3rdparty/SecureSignIn/login-dist/3.SecureSignInLogin.css"
+patch["68b2c5f5747c3085bba5dd628b90ae0f.gif"]="/usr/syno/synoman/webman/3rdparty/SecureSignIn/login-dist/assets/68b2c5f5747c3085bba5dd628b90ae0f.gif"
+patch["addc28996dad298560fd62666efd5c0b.gif"]="/usr/syno/synoman/webman/3rdparty/SecureSignIn/login-dist/assets/addc28996dad298560fd62666efd5c0b.gif"
+
+patch_path="${script_dir}/patch/${patch_name}"
+backups_path="${script_dir}/backups"
+
+# 检查替换相关文件是否都存在
+isexis=''
+for name in ${!patch[*]}
+do
+    if [[ ! -f ${patch_path}/${name} ]]; then
+        echo "【${patch_path}/${name}】补丁文件不存在"
+        isexis='true'
+    fi
+    if [[ ! -f ${patch[${name}]} ]]; then
+        echo "【${patch[${name}]}】系统文件不存在"
+        isexis='true'
+    fi
+done
+
+if [[ -n ${isexis} ]];then
+    echo "文件不全，请检查文件或系统版本是否匹配"
+    exit 1
+fi
+
+function backups() {
+    mkdir ${backups_path}
+    echo "开始备份文件"
+    for name in ${!patch[*]}
+    do
+        cp ${patch[${name}]} ${backups_path}/${name}
+    done
+    mkdir ${backups_path}/${patch_name}
+    echo "文件备份完成，请勿删除《 backups 》目录和目录内文件"
+}
+
+function install() {
+     # 检查是否存在备份文件
+    if [[ -d ${backups_path} ]]; then
+        # 检查是否为当前脚本备份文进
+        if [[ -d ${backups_path}/${patch_name} ]]; then
+            echo "开始替换文件"
+            for name in ${!patch[*]}
+            do
+                cp ${patch_path}/${name} ${patch[${name}]}
+            done
+            echo "替换完成"
+        else
+            echo "已安装不同版本补丁，还原后重新安装"
+        fi
+    else
+        backups
+        echo "开始替换文件"
+        for name in ${!patch[*]}
+        do
+            cp ${patch_path}/${name} ${patch[${name}]}
+        done
+        echo "替换完成"
+    fi
+        
+}
+
+
+function reduction() {
+    # 检查是否存在备份文件
+    if [[ -d ${backups_path} ]]; then
+        # 检查是否为当前脚本备份文进
+        if [[ -d ${backups_path}/${patch_name} ]]; then
+            echo "检查备份文件是否完整"
+            for name in ${!patch[*]}
+            do
+                if [[ ! -f ${backups_path}/${name} ]]; then
+                    echo "${backups_path}/${name}文件不存在"
+                    isexis='true'
+                fi
+            done
+            if [[ -z ${isexis} ]];then
+                echo "开始还原文件"
+                for name in ${!patch[*]}
+                do
+                    cp ${backups_path}/${name} ${patch[${name}]}
+                done
+                # 删除备份
+                rm -rf ${backups_path}  
+                echo "还原完成"
+            else
+                echo "备份文件不全，请检查备份文件或版本是否匹配"
+            fi
+        else
+            echo "当前备份文件不是该脚本生成,请还原后重新安装补丁"
+        fi
+    else
+        echo "不存在备份文件"
+    fi
+}
+while true
+do
+    if [[ -n $par ]]; then
+        input="$par"
+    else
+        read -r -p "1：安装替换补丁 2：还原补丁 q：退出脚本 [1/2/q] " input
+    fi
+    case $input in
+        1)  
+            install
+            ;;
+        2)
+            reduction
+            ;;
+        [qQ])
+            echo "退出脚本"
+            exit 0
+            ;;  
+        *)
+            echo "输入指令错误，重新输入..."
+            ;;
+    esac
+    if [[ -n $par ]]; then
+        exit 0
+    fi
+done
